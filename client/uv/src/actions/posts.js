dbd16c8f95317ab305f4f58832d5bf1527e458c4
Rachel Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-03-30 15:39:36 -0400

    Create posts.js

---
+++ client/uv/src/actions/posts.js
@@ -0,0 +1,56 @@
+import * as api from '../api';
+
+// Action Creators -- return an action
+export const getPosts = () => async(dispatch) => {
+try {
+    const { data } = await api.fetchPosts();
+    dispatch({type: 'FETCH_ALL', payload: data});
+} catch (error) {
+    console.log(error.message);
+}
+}
+
+export const createPost = (post) => async(dispatch) => {
+    try {
+        const {data} = await api.createPost(post);
+        dispatch({type: 'CREATE', payload:data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post) => async(dispatch) => {
+    try {
+        const {data} = await api.updatePost(id, post);
+        dispatch({type: 'UPDATE', payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async(dispatch) => {
+    try {
+        await api.deletePost(id);
+        dispatch({type: 'DELETE', payload: id});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const upvotePost = (id) => async(dispatch) => {
+    try {
+        const {data} = await api.upvotePost(id);
+        dispatch({type: 'UPVOTE', payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const downvotePost = (id) => async(dispatch) => {
+    try {
+        const {data} = await api.downvotePost(id);
+        dispatch({type: 'DOWNVOTE', payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
