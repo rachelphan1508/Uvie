d6c76d08bdbc2acc0f03dac038d8b8a06df35723
Rachel Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-03-30 15:42:15 -0400

    Create postMessage.js

---
+++ server/models/postMessage.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+    // each post needs to have
+    title: String,
+    message: String,
+    creator: String,
+    tags: [String],
+    //selectedFile: String,
+    upvote: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: new Date()
+    },
+
+});
+
+var PostMessage = mongoose.model('PostMessage', postSchema);
+export default PostMessage;
