691ff5bd3ac13d52b21c4a4dac5bee1dffe0ada3
Rachel Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-04-10 19:51:24 -0400

    Create styles.js

---
+++ client/uv/src/components/Posts/styles.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core/styles/index.js';
+
+export default makeStyles((theme) => ({
+  mainContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  smMargin: {
+    margin: theme.spacing(1),
+  },
+  actionDiv: {
+    textAlign: 'center',
+  },
+}));
