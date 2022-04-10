d9ee649e30d195ea4d023f04e14c621a6dd9c9dd
Rachel Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-04-10 19:52:34 -0400

    Create actionTypes.js

---
+++ client/uv/src/constants/actionTypes.js
@@ -0,0 +1,8 @@
+export const CREATE = 'CREATE';
+export const UPDATE = 'UPDATE';
+export const DELETE = 'DELETE';
+export const FETCH_ALL = 'FETCH_ALL';
+export const UPVOTE = 'UPVOTE';
+export const DOWNVOTE = 'DOWNVOTE';
+export const AUTH = 'AUTH';
+export const LOGOUT='LOGOUT';
