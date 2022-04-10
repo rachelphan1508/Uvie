3483b5d4606362c2e330feabf4d731870d3f5330
Rachel Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-04-10 19:48:34 -0400

    Create trending.js

---
+++ client/uv/src/components/trending.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Grid, CircularProgress} from '@material-ui/core/index.js'
+import Post from "./Posts/Post/Post.js";
+
+const Trending = () => {
+    
+    return (
+        <>
+            <h1> Trending Now</h1>
+            
+        </> 
+    );
+}
+export default Trending;
