782f0dae9058bf6d9d3e90e7ff36893b8a41e3b3
Rachel Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-04-11 00:35:44 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ import express from 'express';
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
+
+dotenv.config();
 const app = express();
 
 
@@ -13,6 +17,7 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 
 app.get("/", function(req, res){
@@ -20,15 +25,13 @@ app.get("/", function(req, res){
 });
 
 
+//use mongodb
 
-//use mongodb -- private info in Connection_url
-
-const CONNECTION_URL = 'mongodb+srv://username:passwordcluster0.ewsam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 ;
