556f807170e1e3cd9cb43eb97f0d5451c16a3285
Hanh (Rachel) Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-04-13 18:41:50 -0400

    Add files via upload

--- client/uv/src/api/index.js
+++ client/uv/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: 'http://localhost:5000'});
+const API = axios.create({baseURL: 'https://uvie-backend.herokuapp.com/'});
 
 API.interceptors.request.use((req) => {
     //send token back to backend
