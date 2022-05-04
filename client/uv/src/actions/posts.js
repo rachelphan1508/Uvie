4fd65a4021929ef1d5dfd8818209b919b7c4912c
Hanh (Rachel) Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-05-04 00:20:21 -0400

    Add files via upload

--- client/uv/src/actions/posts.js
+++ client/uv/src/actions/posts.js
@@ -1,19 +1,33 @@
-import * as api from '../api';
+import * as api from '../api/index.js';
+import { CREATE, FETCH_ALL, FETCH_POST, UPDATE, DELETE, UPVOTE, DOWNVOTE, COMMENT } from '../constants/actionTypes.js';
 
 // Action Creators -- return an action
 export const getPosts = () => async(dispatch) => {
 try {
     const { data } = await api.fetchPosts();
-    dispatch({type: 'FETCH_ALL', payload: data});
+    console.log(data);
+    dispatch({type: FETCH_ALL, payload: data});
 } catch (error) {
     console.log(error.message);
 }
 }
 
-export const createPost = (post) => async(dispatch) => {
+//
+export const getPost = (id) => async(dispatch) => {
+    try {
+        const { data } = await api.fetchPost(id);
+        console.log(data);
+        dispatch({type: FETCH_POST, payload: data});
+    } catch (error) {
+        console.log(error.message);
+    }
+    }
+
+export const createPost = (post, navigate) => async(dispatch) => {
     try {
         const {data} = await api.createPost(post);
-        dispatch({type: 'CREATE', payload:data});
+        navigate(`/posts/${data._id}`);
+        dispatch({type: CREATE, payload:data});
     } catch (error) {
         console.log(error);
     }
@@ -22,7 +36,7 @@ export const createPost = (post) => async(dispatch) => {
 export const updatePost = (id, post) => async(dispatch) => {
     try {
         const {data} = await api.updatePost(id, post);
-        dispatch({type: 'UPDATE', payload: data});
+        dispatch({type: UPDATE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -31,7 +45,7 @@ export const updatePost = (id, post) => async(dispatch) => {
 export const deletePost = (id) => async(dispatch) => {
     try {
         await api.deletePost(id);
-        dispatch({type: 'DELETE', payload: id});
+        dispatch({type: DELETE, payload: id});
     } catch (error) {
         console.log(error);
     }
@@ -40,7 +54,7 @@ export const deletePost = (id) => async(dispatch) => {
 export const upvotePost = (id) => async(dispatch) => {
     try {
         const {data} = await api.upvotePost(id);
-        dispatch({type: 'UPVOTE', payload: data});
+        dispatch({type: UPVOTE, payload: data});
     } catch (error) {
         console.log(error);
     }
@@ -49,7 +63,18 @@ export const upvotePost = (id) => async(dispatch) => {
 export const downvotePost = (id) => async(dispatch) => {
     try {
         const {data} = await api.downvotePost(id);
-        dispatch({type: 'DOWNVOTE', payload: data});
+        dispatch({type: DOWNVOTE, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const commentPost = (value, id) => async(dispatch) => {
+    try {
+        const {data} = await api.comment(value, id);
+        
+        dispatch({type: COMMENT, payload:data});
+        return data.comments;
     } catch (error) {
         console.log(error);
     }
