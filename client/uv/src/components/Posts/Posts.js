b7929a17f6e1f7f1cc3f4d377dbe6535cc4bf446
Hanh (Rachel) Phan <49543269+rachelphan1508@users.noreply.github.com>
2022-05-04 00:22:07 -0400

    Add files via upload

--- client/uv/src/components/Posts/Posts.js
+++ client/uv/src/components/Posts/Posts.js
@@ -8,11 +8,12 @@ const Posts = ({setCurrentId}) => {
     const classes = useStyles();
     const posts = useSelector((state) => state.posts);
     console.log(posts);
+    
     return (
         
-        !posts.length ? <CircularProgress/> : ( 
+        !posts.length ? <CircularProgress /> : ( 
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map((post) => (
+                {posts?.map((post) => (
                     <Grid key={post._id} item xs={12} sm={12}>
                         <Post post={post} setCurrentId={setCurrentId}/>
                     </Grid>
